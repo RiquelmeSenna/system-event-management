ac32fafbaf0b3062d99e038c3b2ccaa19f0dca60
Riquelme <riquelmestayler@gmail.com>
2025-01-23 01:22:21 -0300

    configured server

---
+++ src/server.ts
@@ -0,0 +1,21 @@
+import express from 'express';
+import helmet from 'helmet';
+import cors from 'cors';
+import { router } from './routers/mainRouter';
+
+const server = express();
+
+server.use(express.json());
+server.use(express.urlencoded({ extended: true }));
+server.use(express.static('public'));
+server.use(helmet());
+server.use(cors());
+
+
+const port = process.env.PORT || 3000;
+
+server.use('/', router);
+
+server.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+})
