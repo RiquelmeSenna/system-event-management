a2bf5abf61d255e8478dba19fed89381f9bf559a
Riquelme <riquelmestayler@gmail.com>
2025-02-05 19:10:39 -0300

    Done model categories

---
+++ src/models/categories.ts
@@ -0,0 +1,56 @@
+import { prisma } from "../database/prismaConnection"
+
+
+export const getCategories = async () => {
+    const categories = await prisma.category.findMany({
+        include: {
+            Event: true
+        }
+    })
+
+    return categories
+}
+
+export const getCategory = async (id: number) => {
+    const category = await prisma.category.findFirst({
+        where: {
+            id
+        },
+        include: {
+            Event: true
+        }
+    })
+
+    return category
+}
+
+export const getCategoryByname = async (name: string) => {
+    const categories = await prisma.category.findMany({
+        where: { name: { contains: name, mode: 'insensitive' } }
+    })
+}
+
+export const addCategory = async (name: string) => {
+    const category = await prisma.category.create({
+        data: { name }
+    })
+
+    return category
+}
+
+export const updateCategory = async (id: number, name: string) => {
+    const category = await prisma.category.update({ where: { id }, data: { name } })
+
+    return category
+}
+
+export const deletecategory = async (id: number) => {
+    const category = await prisma.category.delete({ where: { id } })
+
+    return category
+}
+
+
+
+
+
